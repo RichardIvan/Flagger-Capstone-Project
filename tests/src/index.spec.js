a29261b2b9120aafd0c4b933166703d5e11584fd
Richard Ivan <richardalfredivan@gmail.com>
2016-07-29 08:27:26 +0000

    add initial test

---
+++ tests/src/index.spec.js
@@ -0,0 +1,11 @@
+/* @flow */
+'use strict'
+
+import { describe, it, beforeEach } from 'mocha'
+import expect from 'expect'
+
+describe('Demo', () => {
+  it('should pass', () => {
+    expect(true).toBe(true)
+  })
+})
