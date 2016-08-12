eea421ea619a365d01cda4166b540f48d958bd85
Richard Ivan <richardalfredivan@gmail.com>
2016-08-12 08:48:05 +0000

    change styling for settings

--- src/js/components/root/index.js
+++ src/js/components/root/index.js
@@ -2,14 +2,14 @@
 'use strict'
 
 import m from 'mithril'
-// import styles from './root-styles.scss'
+import styles from './root-styles.scss'
 import toolbarContainer from '../../containers/Toolbar'
 
 const RootComponent = {
   view(vnode: Object) {
     return m('#root', [
       m(toolbarContainer, { ...vnode.attrs }),
-      m('.container', vnode.attrs.container)
+      m(`.${styles.container}`, vnode.attrs.container)
     ])
   }
 }
