b96bddadf9e82ed5d3967d2c701b2d4591cf894d
Richard Ivan <richardalfredivan@gmail.com>
2016-08-14 19:09:40 +0000

    (fix): conditional load of firebase to fix failing tests

--- src/js/services/firebase.js
+++ src/js/services/firebase.js
@@ -1,25 +1,28 @@
 /* @flow */
 'use strict'
 
-import firebase from 'firebase/app'
-import 'firebase/auth'
-
+// import firebase from 'firebase/app'
+// import 'firebase/auth'
+let firebase
 const options = {
   apiKey: 'AIzaSyCTf3mxrfvE8E1W7HCMhUmyH5Ep4mC0irk',
   // apiKey: '130585767691-e8rk5b3r1uj2sr5og683vgkvod1ge35r.apps.googleusercontent.com',
   authDomain: 'udacity-67253.firebaseapp.com'
 }
 
-var app = firebase.initializeApp(options)
+if (process.env.NODE_ENV !== 'test') {
+  firebase = require('firebase/app')
+  require('firebase/auth')
+
+  var app = firebase.initializeApp(options)
+  const provider = new firebase.auth.GoogleAuthProvider()
+  provider.addScope('https://www.googleapis.com/auth/plus.login')
+}
 
 // console.log(app)
 
-const provider = new firebase.auth.GoogleAuthProvider()
-provider.addScope('https://www.googleapis.com/auth/plus.login')
-
 const Fire = {
   signIn: () => {
-    console.log(firebase.auth())
     return firebase.auth().signInWithPopup(provider)
   },
   signOut: () => {
