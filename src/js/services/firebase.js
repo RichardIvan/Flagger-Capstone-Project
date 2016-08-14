1fc0ffa82a32178445ea00ccfa5e911e59f0528d
Richard Ivan <richardalfredivan@gmail.com>
2016-08-14 19:17:19 +0000

    (fix): fixed scope of provider variable

--- src/js/services/firebase.js
+++ src/js/services/firebase.js
@@ -9,13 +9,14 @@ const options = {
   // apiKey: '130585767691-e8rk5b3r1uj2sr5og683vgkvod1ge35r.apps.googleusercontent.com',
   authDomain: 'udacity-67253.firebaseapp.com'
 }
+let provider
 
 if (process.env.NODE_ENV !== 'test') {
   firebase = require('firebase/app')
   require('firebase/auth')
 
   var app = firebase.initializeApp(options)
-  const provider = new firebase.auth.GoogleAuthProvider()
+  provider = new firebase.auth.GoogleAuthProvider()
   provider.addScope('https://www.googleapis.com/auth/plus.login')
 }
 
